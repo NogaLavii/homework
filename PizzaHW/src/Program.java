c40b7b8e6eac723bf8cf591e688dd6a6c33e97e9
Noga Lavi <nogaley@gmail.com>
2018-10-14 20:00:44 +0300

    Pizza HW fix (added total toppings)

--- PizzaHW/src/Program.java
+++ PizzaHW/src/Program.java
@@ -4,6 +4,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Pizza[] pizzas = new Pizza[5];
+		int totalToppings = 0;
 		for (int i = 0; i < pizzas.length; i++) {
 			pizzas[i] = new Pizza(10.0*(i+1), i+2, i);
 			
@@ -14,7 +15,11 @@ public class Program {
 			else
 				System.out.println("Not basic");
 			pizzas[i].print();
+			totalToppings += pizzas[i].getToppings();
 		}
+		
+		System.out.println();
+		System.out.println("Total toppings: " + totalToppings);
 
 	}
 
